f92cda13f9f0aaa63db56659f5fee168aa7183f3
Clarence Kilcrease <clarencekilcrease@Clarences-MacBook-Pro.local>
2020-09-18 11:00:09 -0400

    created table format

--- js.js
+++ js.js
@@ -1,13 +1,50 @@
-var employeePromise = d3.csv("employee.json");
+var employeePromise = d3.json("employee.json");
 
-var empPromiseSucc = function(employee)
+var empPromiseSucc = function(employees)
     {
-        console.log("Data Successfully Collected!");
+        console.log("Data Successfully Collected!",employees );
+        MakeTable(employees);
     }
 
 var empPromiseFail = function(errorMsg)
     {
-        console.log("Uh oh! Something went wrong!")
+        console.log("Uh oh! Something went wrong!",errorMsgMsg)
     }
 
 employeePromise.then(empPromiseSucc,empPromiseFail);
+
+var MakeTable=function(employee)
+{
+
+var rows=d3.select("#EmpTable tbody")
+.selectAll("tr")
+.data(employee)
+.enter()
+.append("tr")
+
+rows.append("td")
+.text(function(employee){return employee.firstName})
+
+rows.append("td")
+.text(function(employee){return employee.lastName})
+
+rows.append("td")
+    .append("img")
+.attr("src",function(employee){return employee.photo})
+.text(function(employee){return employee.photo})
+
+rows.append("td")
+.text(function(employee){return employee.title })
+
+rows.append("td")
+.text(function(employee){return employee.unit})
+
+rows.append("td")
+.text(function(employee){return employee.email})
+
+rows.append("td")
+.text(function(employee){return employee.bio})
+
+rows.append("td")
+.text(function(employee){return employee.phone})
+}
